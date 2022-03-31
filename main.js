4ffac5d81d8a179ea1ca4194db4338000a146c0b
AarnavR <aarnav.rawat@gmail.com>
2022-03-31 10:26:59 -0400

    Updated slideshow edge cases and trycatch

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+let timer
+let deleteFirstPhotoDelay
+
 async function start() {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all")
-    const data = await response.json()
-    createBreadList(data.message)
+    try {
+        const response = await fetch("https://dog.ceo/api/breeds/list/all")
+        const data = await response.json()
+        createBreadList(data.message)
+    } catch (e) {
+        console.log("There was a problem fetching the breed list.")
+    }
 }
 
 start()
@@ -27,23 +34,34 @@ async function loadByBreed(breed) {
 
 function createSlideShow(images) {
     let currentPosition = 0
-    document.getElementById("slideshow").innerHTML = `
-            <div class="slide" style="background-image: url('${images[0]}')"></div> 
-            <div class="slide" style="background-image: url('${images[1]}')"></div>   
+    clearInterval(timer)
+    clearTimeout(deleteFirstPhotoDelay)
+    //if there is only one image in breed collection
+    if(images.length > 1){
+        document.getElementById("slideshow").innerHTML = `
+        <div class="slide" style="background-image: url('${images[0]}')"></div> 
+        <div class="slide" style="background-image: url('${images[1]}')"></div>   
         `
         currentPosition += 2
+        //if there is only two images in breed collection
+        if(images.length == 2) currentPosition = 0
+        timer = setInterval(nextSlide, 3000)
+    } else {
+        document.getElementById("slideshow").innerHTML = `
+        <div class="slide" style="background-image: url('${images[0]}')"></div> 
+        <div class="slide"></div>   
+        `
+    }
 
-        setInterval(nextSlide, 3000)
-
-        function nextSlide() {
-            document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
-            setTimeout(function() {
-                document.querySelector(".slide").remove()
-            }, 1000)
-            if(currentPosition + 1 >= images.lenght) {
-                currentPosition = 0
-            } else {
-                currentPosition++
-            }
+    function nextSlide() {
+        document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+        deleteFirstPhotoDelay = setTimeout(function() {
+            document.querySelector(".slide").remove()
+        }, 1000)
+        if(currentPosition + 1 >= images.lenght) {
+            currentPosition = 0
+        } else {
+            currentPosition++
         }
+    }
 }
